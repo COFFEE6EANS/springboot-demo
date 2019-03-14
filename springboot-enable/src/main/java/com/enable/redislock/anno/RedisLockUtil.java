1a9557803f48a6a73742b9452ca8be4c32093f11
coffee6eans <coffee6eans@163.com>
2019-03-14 09:13:34 +0800

    分布式redis锁

--- springboot-enable/src/main/java/com/enable/redislock/anno/RedisLockUtil.java
+++ springboot-enable/src/main/java/com/enable/redislock/anno/RedisLockUtil.java
@@ -19,41 +19,42 @@ public class RedisLockUtil {
     private final static String REDISKEY = "REDISKEY";
 
     /**
-     *  获取锁
+     * 获取锁
+     *
      * @param jedis
      * @param connectTime
      * @param expireTime
      * @return
      */
-    public synchronized String tryGetDistributedLock(Jedis jedis, String lockValuePrefix, int connectTime, int expireTime) {
+    public String tryGetDistributedLock(Jedis jedis, String lockValuePrefix, int connectTime, int expireTime) {
 //        log.info("线程{}  。。。",Thread.currentThread().getName());
-        String redisValue = lockValuePrefix+":"+UUID.randomUUID().toString();
+        String redisValue = lockValuePrefix + ":" + UUID.randomUUID().toString();
         //秒->毫秒
-        connectTime = connectTime*1000;
+        connectTime = connectTime * 1000;
         long currentTime = System.currentTimeMillis();
-        while (System.currentTimeMillis() < currentTime+connectTime){
-            String value = jedis.get(REDISKEY);
-            if (StringUtils.isEmpty(value)){
-                log.info("线程{} 获取到锁 成功",Thread.currentThread().getName());
-                jedis.setnx(REDISKEY,redisValue);
-                jedis.expire(REDISKEY,expireTime);
+        while (System.currentTimeMillis() < currentTime + connectTime) {
+            if(jedis.setnx(REDISKEY, redisValue)==1){
+                log.info("线程{} 获取到锁 成功", Thread.currentThread().getName());
+                jedis.expire(REDISKEY, expireTime);
                 return redisValue;
             }
+
         }
-        log.info("线程{} 获取锁失败",Thread.currentThread().getName());
+        log.info("线程{} 获取锁失败", Thread.currentThread().getName());
         return null;
     }
+
     /**
-     *  释放锁
+     * 释放锁
      */
     public boolean tryreleaseDistributedLock(Jedis jedis, String value) {
         String redisLock = jedis.get(REDISKEY);
-        if (StringUtils.equals(redisLock,value)){
+        if (StringUtils.equals(redisLock, value)) {
             jedis.del(REDISKEY);
-            log.info("线程{} 释放锁 成功",Thread.currentThread().getName());
+            log.info("线程{} 释放锁 成功", Thread.currentThread().getName());
             return true;
         }
-        log.info("线程{} 释放锁 失败",Thread.currentThread().getName());
+        log.info("线程{} 释放锁 失败", Thread.currentThread().getName());
         return false;
     }
 
